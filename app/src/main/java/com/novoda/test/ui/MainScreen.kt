e0b2d23528ce34514af08e9d12e085e953b4ec9f
Amin Afshar <ameano@gmail.com>
2026-04-20 12:53:27 +0100

    Added UI views for various states

--- app/src/main/java/com/novoda/test/ui/MainScreen.kt
+++ app/src/main/java/com/novoda/test/ui/MainScreen.kt
@@ -1,12 +1,26 @@
 package com.novoda.test.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.novoda.test.R
 import com.novoda.test.model.User
 import com.novoda.test.ui.theme.NovodaAndroidCodingTestTheme
 
@@ -15,7 +29,7 @@ import com.novoda.test.ui.theme.NovodaAndroidCodingTestTheme
 fun MainScreenEmptyPreview() {
     NovodaAndroidCodingTestTheme {
         MainScreen(
-            state = MainScreenUiState.Empty
+            uiState = MainScreenUiState.Empty
         )
     }
 }
@@ -25,7 +39,7 @@ fun MainScreenEmptyPreview() {
 fun MainScreenLoadingPreview() {
     NovodaAndroidCodingTestTheme {
         MainScreen(
-            state = MainScreenUiState.Loading
+            uiState = MainScreenUiState.Loading
         )
     }
 }
@@ -35,7 +49,7 @@ fun MainScreenLoadingPreview() {
 fun MainScreenErrorPreview() {
     NovodaAndroidCodingTestTheme {
         MainScreen(
-            state = MainScreenUiState.Error("An error occurred")
+            uiState = MainScreenUiState.Error("404 - not found")
         )
     }
 }
@@ -45,7 +59,7 @@ fun MainScreenErrorPreview() {
 fun MainScreenSuccessPreview() {
     NovodaAndroidCodingTestTheme {
         MainScreen(
-            state = MainScreenUiState.Success(
+            uiState = MainScreenUiState.Success(
                 listOf(
                     User(
                         id = 1,
@@ -71,7 +85,7 @@ fun MainScreenRoute(
     }
     MainScreen(
         modifier = modifier,
-        state = uiState.value
+        uiState = uiState.value
     )
 }
 
@@ -79,7 +93,55 @@ fun MainScreenRoute(
 @Composable
 private fun MainScreen(
     modifier: Modifier = Modifier,
-    state: MainScreenUiState
+    uiState: MainScreenUiState
 ) {
-    
+    when (uiState) {
+        MainScreenUiState.Empty -> {
+            Text(
+                modifier = modifier,
+                text = stringResource(R.string.state_empty_title)
+            )
+        }
+
+        is MainScreenUiState.Error -> {
+            Column(modifier = modifier) {
+                Text(text = stringResource(R.string.state_error_title))
+                Text(text = uiState.message)
+            }
+        }
+
+        MainScreenUiState.Loading -> {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        is MainScreenUiState.Success -> {
+            LazyColumn(modifier = modifier.padding(16.dp)) {
+                items(uiState.users) { user ->
+                    UserItem(
+                        modifier = Modifier.fillMaxWidth(),
+                        user = user
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserItem(
+    modifier: Modifier = Modifier,
+    user: User
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text("Name: ${user.name}")
+        Text("Reputation: ${user.reputation}")
+        Text("Image: ${user.imageUrl}")
+        Button(onClick = {}) {
+            Text(text = stringResource(R.string.button_follow))
+        }
+    }
 }
